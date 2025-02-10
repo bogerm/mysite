18ef6a4d13f02d1d2aa8945593122d539eb5074a
Oleg Buklanov <buklanov@gmail.com>
2025-02-10 21:03:32 +0000

    Add movies model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Moviedata(models.Model):
+    name = models.CharField(max_length=200)
+    duration = models.FloatField()
+    rating = models.FloatField()
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -32,8 +32,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
     'food.apps.FoodConfig',
     'users.apps.UsersConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
