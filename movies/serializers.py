e12237e1e9db2842213fbdeb24aaf00ea494393f
Oleg Buklanov <buklanov@gmail.com>
2025-02-11 21:26:15 +0000

    Add images

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,5 +3,6 @@ from .models import Moviedata
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
+        image = serializers.ImageField(max_length=None, use_url=True)
         model = Moviedata
-        fields = ['id', 'name', 'duration', 'rating', 'movie_type']
+        fields = ['id', 'name', 'duration', 'rating', 'movie_type', 'image']
