3d0f80cd2d00ed2e6175e8031ec77ac5de40b624
Oleg Buklanov <buklanov@gmail.com>
2025-02-11 21:17:40 +0000

    Add endpoints

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from movies.views import MoviesViewSet
+from movies.views import MoviesViewSet, ActionViewSet, ComedyViewSet
 
-router = routers.DefaultRouter()
-router.register('', MoviesViewSet)
+router = routers.SimpleRouter()
+router.register('all', MoviesViewSet, basename='all_movies_router')
+router.register('action', ActionViewSet, basename='action_movies_router')
+router.register('comedy', ComedyViewSet, basename='comedy_movies_router')
 
 urlpatterns = [
     path('', include(router.urls))
--- movies/views.py
+++ movies/views.py
@@ -6,3 +6,11 @@ from .models import Moviedata
 class MoviesViewSet(viewsets.ModelViewSet):
     queryset = Moviedata.objects.all()
     serializer_class = MovieSerializer
+
+class ActionViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(movie_type='action')
+    serializer_class = MovieSerializer
+
+class ComedyViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(movie_type='comedy')
+    serializer_class = MovieSerializer
